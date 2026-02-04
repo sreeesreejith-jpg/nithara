d757224fec654a2b0b4bf76c213c5b6756882e0b
Sreejith <sreee.sreejith@gmail.com>
2026-02-04 12:55:03 +0530

    Brief disclaimer and remove redundant Employee Details header

--- pay-revision/script.js
+++ pay-revision/script.js
@@ -1695,7 +1695,7 @@ document.addEventListener('DOMContentLoaded', () => {
             doc.rect(0, 0, 210, 45, 'F');
             doc.setFontSize(8);
             doc.setTextColor(255);
-            doc.text("* NOTE: Calculations are approximate and for informational purposes only.", 14, 12);
+            doc.text("* Approx. values for info only.", 14, 12);
 
             doc.setFontSize(22);
             doc.setTextColor(255);
